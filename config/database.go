8ab03c0317e311f0411ae6eb9be52e61df5c7675
Bosco <jbosco0308@gmail.com>
2024-12-04 14:17:36 +0700

    update

--- config/database.go
+++ config/database.go
@@ -2,9 +2,12 @@ package config
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var DB *sql.DB
 
 func ConnectDB() {
 	db, err := sql.Open("mysql", "user:password@/dbname")
@@ -12,4 +15,6 @@ func ConnectDB() {
 		panic(err)
 	}
 
+	log.Println("Database connected")
+	DB = db
 }
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"go-web-native/config"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.ConnectDB()
+
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
