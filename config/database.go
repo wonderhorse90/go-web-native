2b9232b4fce7a389f52c2faddbfb73dfe993ea3c
Bosco <jbosco0308@gmail.com>
2024-12-04 13:54:06 +0700

    first commit

---
+++ config/database.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectDB() {
+	db, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		panic(err)
+	}
+
+}
